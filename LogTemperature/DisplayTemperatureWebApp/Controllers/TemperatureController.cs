c4dcad35bebd47c7658c55211215619e1369fc93
Jeff Evans <reznet@gmail.com>
2013-01-03 11:32:12 -0800

    add temperature controller

---
+++ LogTemperature/DisplayTemperatureWebApp/Controllers/TemperatureController.cs
@@ -0,0 +1,39 @@
+﻿using DisplayTemperatureWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DisplayTemperatureWebApp.Controllers
+{
+    public class TemperatureController : ApiController
+    {
+        public IEnumerable<TemperatureMeasurement> GetTemperatures()
+        {
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand();
+                command.Connection = connection;
+                command.CommandType = System.Data.CommandType.TableDirect;
+                command.CommandText = "dbo.TemperatureMeasurement";
+
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    yield return new TemperatureMeasurement()
+                    {
+                        TemperatureMeasurementId = Convert.ToInt32(reader["TemperatureMeasurementId"]),
+                        TemperatureCelcius = Convert.ToDouble(reader["TemperatureCelcius"]),
+                        MeasurementTimestamp = DateTimeOffset.Parse(Convert.ToString(reader["MeasurementTimestamp"]))
+                    };
+                }
+            }
+        }
+    }
+}
---
+++ LogTemperature/DisplayTemperatureWebApp/Models/TemperatureMeasurement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DisplayTemperatureWebApp.Models
+{
+    public class TemperatureMeasurement
+    {
+        public int TemperatureMeasurementId { get; set; }
+
+        public double TemperatureCelcius { get; set; }
+
+        public DateTimeOffset MeasurementTimestamp { get; set; }
+    }
+}
