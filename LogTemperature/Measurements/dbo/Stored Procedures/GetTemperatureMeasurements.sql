840c0961c117d2955b50149d16d593aac626cdf5
Jeff Evans <reznet@gmail.com>
2013-01-05 18:39:41 -0800

    change sproc which gets temperature measurements to return data in ascending order so website doesn't need to reverse data
    add ssdt publish profile, target sql 2008

--- LogTemperature/Measurements/dbo/Stored Procedures/GetTemperatureMeasurements.sql
+++ LogTemperature/Measurements/dbo/Stored Procedures/GetTemperatureMeasurements.sql
@@ -4,5 +4,4 @@ BEGIN
 	SELECT *, 32 + (1.8 * TemperatureCelcius) AS TemperatureFahrenheit
 	FROM dbo.TemperatureMeasurement
 	WHERE MeasurementTimestamp > DATEADD(Day, -1, SYSDATETIMEOFFSET())
-	ORDER BY TemperatureMeasurementId DESC
 END
