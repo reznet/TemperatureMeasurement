c6582bebc9e127c213e96dd8477ab71a0b1aee7d
Jeff Evans <reznet@gmail.com>
2013-01-05 18:26:21 -0800

    import Measurement database as SSDT project

---
+++ LogTemperature/Measurements/dbo/Stored Procedures/GetTemperatureMeasurements.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROC [dbo].[GetTemperatureMeasurements]
+AS
+BEGIN
+	SELECT *, 32 + (1.8 * TemperatureCelcius) AS TemperatureFahrenheit
+	FROM dbo.TemperatureMeasurement
+	WHERE MeasurementTimestamp > DATEADD(Day, -1, SYSDATETIMEOFFSET())
+	ORDER BY TemperatureMeasurementId DESC
+END
---
+++ LogTemperature/Measurements/dbo/Tables/TemperatureMeasurement.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[TemperatureMeasurement] (
+    [TemperatureMeasurementId] INT                IDENTITY (1, 1) NOT NULL,
+    [TemperatureCelcius]       DECIMAL (18, 4)    NOT NULL,
+    [MeasurementTimestamp]     DATETIMEOFFSET (7) NOT NULL
+);
+
