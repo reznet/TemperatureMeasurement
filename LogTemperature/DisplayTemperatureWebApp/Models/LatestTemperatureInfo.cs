332600f48849d5ada90870a5f824df078377c5ef
Jeff Evans <reznet@gmail.com>
2013-08-26 23:26:45 -0700

    add LatestTemperatureInfo class

---
+++ LogTemperature/DisplayTemperatureWebApp/Models/LatestTemperatureInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DisplayTemperatureWebApp.Models
+{
+    public class LatestTemperatureInfo
+    {
+        public string SourceName { get; set; }
+
+        public double TemperatureFahrenheit { get; set; }
+    }
+}
