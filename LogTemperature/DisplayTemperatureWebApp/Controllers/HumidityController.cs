6c4ede5a0e1a9aeb875877bf6cad3e8dce349165
Jeff Evans <reznet@gmail.com>
2014-04-03 20:17:13 -0700

    placeholder humidity controller and model

---
+++ LogTemperature/DisplayTemperatureWebApp/Controllers/HumidityController.cs
@@ -0,0 +1,30 @@
+﻿using DisplayTemperatureWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DisplayTemperatureWebApp.Controllers
+{
+    public class HumidityController : ApiController
+    {
+        // GET api/humidity
+        public IEnumerable<HumidityMeasurement> Get()
+        {
+            return new HumidityMeasurement[0];
+        }
+
+        // GET api/humidity/5
+        public HumidityMeasurement Get(int id)
+        {
+            return null;
+        }
+
+        // POST api/humidity
+        public void Post([FromBody]HumidityMeasurement value)
+        {
+        }
+    }
+}
