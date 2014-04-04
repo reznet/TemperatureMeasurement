1b310ee893df2dca15d75b2c4773f3c8678fc374
Jeff Evans <reznet@gmail.com>
2014-04-03 20:58:22 -0700

    log humidity console app

---
+++ LogTemperature/LogHumidity/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogHumidity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double value;
+            var sensor = new WatchportHumiditySensor("COM3", TimeSpan.FromSeconds(5));
+
+            try
+            {
+                value = sensor.Read();
+                Console.WriteLine(value);
+            }
+            catch(TimeoutException timeoutException)
+            {
+                Console.Error.WriteLine(timeoutException);
+                Environment.ExitCode = 1;
+            }
+        }
+    }
+}
