a02ff8610b0623c15264d66d98da1837371706af
Jeff Evans <reznet@gmail.com>
2014-04-03 20:26:53 -0700

    create proc GetHumidityMeasurements

---
+++ LogTemperature/Measurements/dbo/Stored Procedures/GetHumidityMeasurements.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROC [dbo].[GetHumidityMeasurements]
+AS
+BEGIN
+	SELECT h.HumidityMeasurementId, h.HumidityPercentage, h.MeasurementTimestamp, h.SourceName
+	FROM dbo.HumidityMeasurement AS h
+	WHERE MeasurementTimestamp > DATEADD(Day, -1, SYSDATETIMEOFFSET())
+	ORDER BY h.HumidityMeasurementId
+END
