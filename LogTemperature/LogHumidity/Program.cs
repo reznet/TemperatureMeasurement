7ffb8d6374f84d18d2d192185be0a5f04999374e
Jeff Evans <reznet@gmail.com>
2014-07-03 21:13:19 -0700

    wrap humidity program in try/catch so scheduled tasks are not interrupted when the machine can't access the azure web site

--- LogTemperature/LogHumidity/Program.cs
+++ LogTemperature/LogHumidity/Program.cs
@@ -12,50 +12,57 @@ namespace LogHumidity
     {
         static void Main(string[] args)
         {
-            string comPortName = ConfigurationManager.AppSettings["ComPortName"];
-            if(string.IsNullOrWhiteSpace(comPortName))
-            {
-                Console.Error.WriteLine("ComPortName must be set in the .config file.");
-                Environment.Exit(1);
-            }
+            try {
+                string comPortName = ConfigurationManager.AppSettings["ComPortName"];
+                if (string.IsNullOrWhiteSpace(comPortName))
+                {
+                    Console.Error.WriteLine("ComPortName must be set in the .config file.");
+                    Environment.Exit(1);
+                }
 
-            string logWebsiteUrlValue = ConfigurationManager.AppSettings["LogWebsiteUri"];
-            if (string.IsNullOrWhiteSpace(logWebsiteUrlValue))
-            {
-                Console.Error.WriteLine("LogWebsiteUri must be set in the .config file.");
-                Environment.Exit(1);
-            }
+                string logWebsiteUrlValue = ConfigurationManager.AppSettings["LogWebsiteUri"];
+                if (string.IsNullOrWhiteSpace(logWebsiteUrlValue))
+                {
+                    Console.Error.WriteLine("LogWebsiteUri must be set in the .config file.");
+                    Environment.Exit(1);
+                }
 
-            string sourceNameValue = ConfigurationManager.AppSettings["SourceName"];
-            if (string.IsNullOrWhiteSpace(sourceNameValue))
-            {
-                Console.Error.WriteLine("SourceName must be set in the .config file.");
-                Environment.Exit(1);
-            }
+                string sourceNameValue = ConfigurationManager.AppSettings["SourceName"];
+                if (string.IsNullOrWhiteSpace(sourceNameValue))
+                {
+                    Console.Error.WriteLine("SourceName must be set in the .config file.");
+                    Environment.Exit(1);
+                }
 
-            string readTimeoutSecondsSetting = ConfigurationManager.AppSettings["ReadTimeoutSeconds"];
-            int readTimeoutSeconds = 5;
-            if(!string.IsNullOrWhiteSpace(readTimeoutSecondsSetting))
-            {
-                readTimeoutSeconds = int.Parse(readTimeoutSecondsSetting);
-            }
+                string readTimeoutSecondsSetting = ConfigurationManager.AppSettings["ReadTimeoutSeconds"];
+                int readTimeoutSeconds = 5;
+                if (!string.IsNullOrWhiteSpace(readTimeoutSecondsSetting))
+                {
+                    readTimeoutSeconds = int.Parse(readTimeoutSecondsSetting);
+                }
 
-            double value = 0;
-            var sensor = new WatchportHumiditySensor(comPortName, TimeSpan.FromSeconds(readTimeoutSeconds));
+                double value = 0;
+                var sensor = new WatchportHumiditySensor(comPortName, TimeSpan.FromSeconds(readTimeoutSeconds));
 
-            try
-            {
-                value = sensor.Read();
+                try
+                {
+                    value = sensor.Read();
+                }
+                catch (Exception exception)
+                {
+                    Console.Error.WriteLine(exception);
+                    Environment.Exit(1);
+                }
+
+                var sink = new MeasurementSink(new Uri(logWebsiteUrlValue), sourceNameValue);
+
+                sink.RecordMeasurement(value).Wait();
             }
             catch(Exception exception)
             {
                 Console.Error.WriteLine(exception);
                 Environment.Exit(1);
             }
-
-            var sink = new MeasurementSink(new Uri(logWebsiteUrlValue), sourceNameValue);
-
-            sink.RecordMeasurement(value).Wait();
         }
     }
 }
