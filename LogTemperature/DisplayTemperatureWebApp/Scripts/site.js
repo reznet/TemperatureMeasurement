1a14aff162544d47dbec5ffc229a5ebb986f48f5
Jeff Evans <reznet@gmail.com>
2014-07-27 16:12:46 -0700

    faster home page loading.  loads temperature and humidity data in parallel instead of sequentially

--- LogTemperature/DisplayTemperatureWebApp/Scripts/site.js
+++ LogTemperature/DisplayTemperatureWebApp/Scripts/site.js
@@ -44,97 +44,117 @@ function loadLatestTemperatures() {
                 });
 }
 
+function getChartDataFromTemperatures(data){
+    var series = {};
+
+    $.each(data, function (key, val) {
+        var source = val.Source;
+        var items;
+        if (!series[source]) {
+            series[source] = {
+                name: source,
+                data: []
+            };
+        }
+        items = series[source];
+        var d = new Date(val.MeasurementDateTimeUtc);
+        items.data.push([Date.UTC(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds()), val.TemperatureFahrenheit]);
+    });
+
+    var data = [];
+    for (var s in series) {
+        data.push(series[s]);
+    }
+
+    return data;
+}
+
+function getChartDataFromHumidities(data){
+    var series = {};
+
+    $.each(data, function (key, val) {
+        var source = val.Source + " (Humidity)";
+        var items;
+        if (!series[source]) {
+            series[source] = {
+                name: source,
+                data: [],
+                yAxis: 1,
+            };
+        }
+        items = series[source];
+        var d = new Date(val.MeasurementDateTimeUtc);
+        items.data.push([Date.UTC(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds()), val.HumidityPercentage]);
+    });
+
+    var data = [];
+    for (var s in series) {
+        data.push(series[s]);
+    }
+
+    return data;
+}
+
+function renderChart(containerId, data){
+    var chart = new Highcharts.Chart({
+        chart: {
+            renderTo: containerId,
+            type: 'spline',
+            zoomType: 'x'
+        },
+        title: {
+            text: 'Last 24 Hours'
+        },
+        xAxis: {
+            type: 'datetime',
+            dateTimeLabelFormats: {
+                hour: '%I:%M %p'
+            },
+            maxZoom: 24 * 3600000 // 1 day
+        },
+        yAxis: [
+            { title: { text: 'Fahrenheit' } },
+            { title: { text: '%' }, opposite: true },
+        ],
+        tooltip: {
+            formatter: function () {
+                return this.series.name + '<br/>' + Highcharts.dateFormat("%l:%M %p", this.x, true) + '<br/>' + this.y + '°F';
+            }
+        },
+        plotOptions: {
+            spline: {
+                lineWidth: 4,
+                states: {
+                    hover: {
+                        lineWidth: 5
+                    }
+                },
+                marker: {
+                    enabled: false
+                }
+            }
+        },
+        legend: {
+            enabled: true
+        },
+        series: data
+    });
+}
+
 function fillTemperatureChart() {
-    $.getJSON("api/temperature/",
-                    function (data) {
-                        var series = {};
-
-                        $.each(data, function (key, val) {
-                            var source = val.Source;
-                            var items;
-                            if (!series[source]) {
-                                series[source] = {
-                                    name: source,
-                                    data: []
-                                };
-                            }
-                            items = series[source];
-                            var d = new Date(val.MeasurementDateTimeUtc);
-                            items.data.push([Date.UTC(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds()), val.TemperatureFahrenheit]);
-                        });
-
-                        var data = [];
-                        for (var s in series) {
-                            data.push(series[s]);
-                        }
-
-                        var chart = new Highcharts.Chart({
-                            chart: {
-                                renderTo: 'container',
-                                type: 'spline',
-                                zoomType: 'x'
-                            },
-                            title: {
-                                text: 'Last 24 Hours'
-                            },
-                            xAxis: {
-                                type: 'datetime',
-                                dateTimeLabelFormats: {
-                                    hour: '%I:%M %p'
-                                },
-                                maxZoom: 24 * 3600000 // 1 day
-                            },
-                            yAxis: [
-                                { title: { text: 'Fahrenheit' } },
-                                { title: { text: '%' }, opposite: true },
-                            ],
-                            tooltip: {
-                                formatter: function () {
-                                    return this.series.name + '<br/>' + Highcharts.dateFormat("%l:%M %p", this.x, true) + '<br/>' + this.y + '°F';
-                                }
-                            },
-                            plotOptions: {
-                                spline: {
-                                    lineWidth: 4,
-                                    states: {
-                                        hover: {
-                                            lineWidth: 5
-                                        }
-                                    },
-                                    marker: {
-                                        enabled: false
-                                    }
-                                }
-                            },
-                            legend: {
-                                enabled: true
-                            },
-                            series: data
-                        });
-
-                        $.getJSON("api/humidity/",
-                            function (data) {
-                                var humiditySeries = {};
-
-                                $.each(data, function (key, val) {
-                                    var source = val.Source + " (Humidity)";
-                                    var items;
-                                    if (!humiditySeries[source]) {
-                                        humiditySeries[source] = {
-                                            name: source,
-                                            data: [],
-                                            yAxis: 1,
-                                        };
-                                    }
-                                    items = humiditySeries[source];
-                                    var d = new Date(val.MeasurementDateTimeUtc);
-                                    items.data.push([Date.UTC(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds()), val.HumidityPercentage]);
-                                });
-
-                                var humidityData = [];
-                                for (var s in humiditySeries) {
-                                    chart.addSeries(humiditySeries[s]);
-                                }
-                            });
-                    });
+    // make two async calls to get temperatures and humidities
+    // when each call finishes, store the result in a local variable
+    // when both calls have finished, parse the results and then send them to
+    // the chart rendering function
+    var temperatures = [];
+    var humidities = [];
+    $.when($.getJSON("api/temperature/", function (data) {
+        temperatures = data;
+    }), $.getJSON("api/humidity/", function (data) {
+        humidities = data;
+    })).done(function(){
+        var temperatureData = getChartDataFromTemperatures(temperatures);
+        var humidityData = getChartDataFromHumidities(humidities);
+        renderChart('container', temperatureData.concat(humidityData));
+    });
 }
