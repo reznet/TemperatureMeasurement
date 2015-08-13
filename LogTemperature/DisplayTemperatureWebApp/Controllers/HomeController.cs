d8256d12dc2b0ccdc1868e48eabc13a99f1135c9
Jeff Evans <reznet@gmail.com>
2015-08-12 23:29:38 -0500

    Add "Inline" view and viewmodel
    Creates a new View and ViewModel that renders the home page as static html instead of dynamically loading the data via REST

--- LogTemperature/DisplayTemperatureWebApp/Controllers/HomeController.cs
+++ LogTemperature/DisplayTemperatureWebApp/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DisplayTemperatureWebApp.Repositories;
+using DisplayTemperatureWebApp.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,25 @@ namespace DisplayTemperatureWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly MeasurementRepository m_measurementRepository;
+
+        public HomeController()
+        {
+            m_measurementRepository = new MeasurementRepository();
+        }
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Inline()
+        {
+            var viewModel = new InlineViewModel();
+
+            viewModel.LatestTemperatures = m_measurementRepository.GetLatestTemperatureInfos();
+
+            return View(viewModel);
         }
     }
 }
---
+++ LogTemperature/DisplayTemperatureWebApp/ViewModels/InlineViewModel.cs
@@ -0,0 +1,13 @@
+﻿using DisplayTemperatureWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DisplayTemperatureWebApp.ViewModels
+{
+    public class InlineViewModel
+    {
+        public IEnumerable<LatestTemperatureInfo> LatestTemperatures { get; set; } 
+    }
+}
